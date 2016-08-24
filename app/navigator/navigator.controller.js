e417d872f713a21bdfdce2f3131805fab36ee06e
Daniel Essig <dessig@godaddy.com>
2016-08-23 23:17:27 -0700

    way better looking!

--- app/navigator/navigator.controller.js
+++ app/navigator/navigator.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = NavigatorController;
-NavigatorController.$inject = ['$scope', 'notes'];
+NavigatorController.$inject = ['$mdDialog', '$scope', 'notes'];
 
-function NavigatorController ($scope, notes) {
+function NavigatorController ($mdDialog, $scope, notes) {
   var ctrl = this;
   ctrl.selectedIndex = -1;
   ctrl.loading = true;
@@ -17,14 +17,42 @@ function NavigatorController ($scope, notes) {
       return {
         _id: d._id,
         title: d.title,
-        updated: d.updated
+        updated: d.updated,
+        menuOpen: false
       };
     });
   });
 
+  // activate note
   ctrl.makeActive = notes.makeActive;
 
+  // remove note
   ctrl.removeNote = function removeNote(id) {
     notes.deleteNote(id);
+  };
+
+  // remove all notes
+  ctrl.deleteAllNotes = function deleteAllNotes(ev) {
+    var confirm = $mdDialog.confirm()
+      .title('Delete all notes?')
+      .textContent('This action is not reversable.')
+      .ariaLabel('Delete all notes')
+      .targetEvent(ev)
+      .ok('Yes, delete everything.')
+      .cancel('NO!');
+    $mdDialog.show(confirm).then(notes.deleteAllNotes);
+  }
+
+  // create new note
+  ctrl.showNewNote = function showNewNote(ev) {
+    var confirm = $mdDialog.prompt()
+      .title('Create new note')
+      .textContent('Please provide a title for your new note.')
+      .ariaLabel('Create new note')
+      .targetEvent(ev)
+      .ok('Okay')
+      .cancel('Cancel');
+
+    $mdDialog.show(confirm).then(notes.insertNote);
   };
 }
